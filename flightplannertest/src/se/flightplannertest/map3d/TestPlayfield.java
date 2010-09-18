3db5a398a5e1197b9c5333e476ece71e53612a8c
Anders Musikka <anders.musikka@gmail.com>
2010-09-18 23:06:33 +0200

    more work on test

--- flightplannertest/src/se/flightplannertest/map3d/TestPlayfield.java
+++ flightplannertest/src/se/flightplannertest/map3d/TestPlayfield.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import junit.framework.Assert;
+
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Test;
@@ -32,8 +34,10 @@ public class TestPlayfield {
 	@Test
 	public void testPlayfieldVirtualThing() throws FileNotFoundException, IOException
 	{
-		iMerc p1=Project.latlon2imerc(new LatLon(59,17), 13);
-		iMerc p2=Project.latlon2imerc(new LatLon(57,19), 13);
+		final int pgap=13-5;
+		final int parentsize=64<<pgap;
+		iMerc p1=new iMerc(0,0);
+		iMerc p2=new iMerc(parentsize*2,parentsize);
 		VertexStore vstore=new VertexStore(100);
 		ElevationStore estore=TestElevMap.getSampleEstore();
 		TriangleStore tristore=new TriangleStore(100);
@@ -48,7 +52,7 @@ public class TestPlayfield {
 		children[3]=mock.mock(ThingIf.class,"child3");
 		final ThingIf[] allmocks=new ThingIf[6];
 		final iMerc observerpos=new iMerc(0,0);
-		final int observerheight=1000;
+		final short observerheight=1000;
 		allmocks[0]=parent;
 		allmocks[1]=neighbor;
 		final ThingIf[] parents=new ThingIf[]{parent,neighbor};
@@ -59,61 +63,61 @@ public class TestPlayfield {
 	        mock.checking(new Expectations() {{
 	        	allowing(t).bumpiness();will(returnValue(100.0f));
         		allowing(t).getZoomlevel();will(returnValue(5));
+	        	allowing(t).isSubsumed();will(returnValue(false));
         		if (t==parent)
         		{
         			//the parent is close, to absolutely positively force a refine.
-        			allowing(t).getDistance(observerpos,observerheight);will(returnValue(0));
+        			allowing(t).getDistance(observerpos,observerheight);will(returnValue(0.0f));
         		}
         		else
         		{
         			//make the non-parent be very very far away
-        			allowing(t).getDistance(observerpos,observerheight);will(returnValue(64<<13));
+        			allowing(t).getDistance(observerpos,observerheight);will(returnValue((float)(64<<13)));
         		}
         		
 	        }});
 		}
-		int cnt=0;
+		int chcnt=0;
 		for(final ThingIf t:children)
 		{
-			final int dx=cnt%2;
-			final int dy=cnt/2;
+			final int dx=chcnt%2;
+			final int dy=chcnt/2;
+			++chcnt;
 			final int gap=13-6;
 			final int childsize=64<<gap;
 	        mock.checking(new Expectations() {{
 	        	allowing(t).bumpiness();will(returnValue(0.0f));
+	        	allowing(t).isSubsumed();will(returnValue(false));
         		allowing(t).getZoomlevel();will(returnValue(6));
     			allowing(t).getPos();will(returnValue(new iMerc(dx*childsize,dy*childsize)));
+    			allowing(t).getDistance(observerpos,observerheight);will(returnValue((float)(2.0*childsize)));
 
 	        }});
-	        ++cnt;
 		}
 		
-		
+		final int[] cnt=new int[1];
 		ThingFactory thingf=new ThingFactory()
 		{
-			int cnt;
 			@Override
 			public ThingIf createThing(VertexStore vstore, ElevationStore estore,
 					int i, iMerc m,Stitcher st) {
-				++cnt;
-				if (cnt==0) return parent;
-				if (cnt==1) return neighbor;
-				if (cnt<6)
-					return children[cnt-2];
+				++cnt[0];
+				if (cnt[0]-1==0) return parent;
+				if (cnt[0]-1==1) return neighbor;
+				if (cnt[0]-1<6)
+					return children[cnt[0]-2];
 				throw new RuntimeException("Created more things than expected");
 			}
 		};
-		for(int i=0;i<6;++i)
-        mock.checking(new Expectations() {{
-        	oneOf(parent).bumpiness();will(returnValue(100.0f));
-        }});
         System.out.println("Bumpiness:"+parent.bumpiness());
         System.out.println("Unexpected:"+parent.getZoomlevel());
-		/*
+		
 		Playfield play=new Playfield(p1,p2,vstore,tristore,lc,estore,thingf);
-		iMerc observer=Project.latlon2imerc(new LatLon(57.5,18.4), 13);
+		Assert.assertEquals(2,cnt[0]);
+		iMerc observer=new iMerc(0,0);
+		
 		play.changeLods(observer, (short)1000, vstore, estore);
-		*/
+		Assert.assertEquals(6,cnt[0]);
 		mock.assertIsSatisfied();
 	}
 	
