8f8c60143aa83a6c85b94cbe52be92e1a7db4d90
Anders Musikka <anders.musikka@gmail.com>
2011-01-25 09:20:45 +0100

    Declutter, missed file

---
+++ flightplanner/src/se/flightplanner/DeclutterTree.java
@@ -0,0 +1,43 @@
+package se.flightplanner;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+import android.graphics.Rect;
+import android.util.Log;
+
+public class DeclutterTree {
+
+	
+	TreeSet<Rect> objs;
+	static public class DeclutterComparator implements Comparator<Rect>
+	{
+		@Override
+		public int compare(Rect a, Rect b) {
+			if (a.top<b.top) return -1;
+			if (a.top>b.top) return +1;
+			if (a.left<b.left) return -1;
+			if (a.left>b.left) return +1;
+			return 0;
+		}		
+	}
+	int maxtextsize;
+	public DeclutterTree(int maxtextsize)
+	{
+		this.maxtextsize=maxtextsize;
+		objs=new TreeSet<Rect>(new DeclutterComparator());
+	}
+	public boolean checkAndAdd(Rect cur)
+	{
+		for(Rect o : objs.subSet(new Rect(0,cur.top-maxtextsize+1,0,0), new Rect(0,cur.bottom+maxtextsize,0,0)))
+		{
+			if (Rect.intersects(o,cur))
+			{
+				return false;
+			}
+		}
+		objs.add(cur);
+		return true;
+	}
+	
+}
